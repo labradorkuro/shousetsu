0d8805d53e933fcc93003737d7f3fd4d900dfd39
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2017-01-21 16:17:18 +0900

    POST先URLを本番環境に変更。

--- shousetsu.py
+++ shousetsu.py
@@ -25,7 +25,7 @@ g_cmpTime = 0			#時間経過比較用時刻
 port = 0
 config_file = '/home/pi/sensor_config.txt'
 
-url = 'http://192.168.100.12:3000/sensor_post'
+url = ''
 #url = 'http://157.7.222.217:8081/nsl1.php'
 #url = 'http://kcs2000.info/remos/ajaxlib.php'
 
@@ -79,7 +79,7 @@ def getDatetime():
 def readData():
     global port
     values = []
-    port.write('req\r\n')
+    port.write('req\r')
     str = port.readline()  # 行終端'\n'までリードする
     print "取得データ: %s " % str + "[len=%d]" % len(str)
     if len(str) > 0:
@@ -97,6 +97,7 @@ def main():
     config = readConfig(config_file)
     sensor_no = getConfig(config,"sensor_no")
     serial_port = getConfig(config,"serial_port")
+    url = getConfig(config,"post_url")
     g_cmpTime = time.time()
 
     #g_macAddr = getMacAddr("wwan0")
